4933f0a8883651533d4aa80f9992f36cd5bec26c
Dan P <73758508+benochi@users.noreply.github.com>
2021-06-23 13:42:17 -0500

    Create square_digits.py

---
+++ square_digits.py
@@ -0,0 +1,18 @@
+def square_digits(num):
+    digits =[]
+    number = num
+    if num != 0:
+        while number > 0:
+            last_digit = number % 10
+            squared = last_digit * last_digit
+            digits.insert(0, squared)
+            number = (number - number % 10) // 10
+        strings = [str(digit) for digit in digits]    
+        a_string = "".join(strings)
+        an_integer = int(a_string)
+        return an_integer
+    else:
+        return num
+
+square_digits(9119) #return 811181
+square_digits(0) #return 0
